0071a9ff638efddbe2e524785a6e4efea3ee2db1
hemlok <adam.seckel@gmail.com>
2017-10-05 14:55:32 +0100

    use https

--- src/services/openWeatherRequestService.js
+++ src/services/openWeatherRequestService.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export default function(apiKey = '2cf1dc4180d34b998f4efbd8fad4a350', city = 'London', countryCode = 826) {
   const openWeatherRequest = axios.create({
-    baseURL: `http://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${apiKey}&units=metric`
+    baseURL: `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${apiKey}&units=metric`
   });
 
   const localWeatherRequest = axios.create({
